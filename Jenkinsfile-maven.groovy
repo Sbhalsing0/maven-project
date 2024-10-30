2fe3caa236dca0edda5a4dacfe8eb355be3a6e96
Sanket Bhalsing <42617962+Sbhalsing0@users.noreply.github.com>
2024-10-30 22:06:02 +0530

    Update Jenkinsfile-maven.groovy

--- Jenkinsfile-maven.groovy
+++ Jenkinsfile-maven.groovy
@@ -6,9 +6,10 @@ pipeline {
         apiVersion: v1
         kind: Pod
         spec:
+          serviceAccountName: jenkins
           containers:
           - name: slave
-            image: sbhalsing0/jenkins-docker-desktop:v1.2
+            image: sbhalsing0/jenkins-docker-desktop:v1.4
             command:
             - cat
             securityContext: 
@@ -23,21 +24,40 @@ pipeline {
               path: /var/run/docker.sock
               type: Socket
         """
-        }
-    }
+      }
+  }
+  options {
+    timestamps()
+    buildDiscarder(logRotator(numToKeepStr: '40', daysToKeepStr: '14', artifactDaysToKeepStr: '7', artifactNumToKeepStr: '10'))
+    timeout(time: 3, unit:'HOURS')
+  }
     stages {
         stage('Build') {
             steps {
-              sh 'echo $BUILD_ID'
-              sh 'java -version'
-              sh 'ls -la'
+                script {
+                    currentBuild.displayName = "#${BUILD_NUMBER} - Build Image"
+                    currentBuild.description = 'Image to build '
+                }
+                sh '''
+                echo $BUILD_ID
+                mvn clean install -DskipTests=false
+                '''
             }
         }
         stage('Release') {
             steps {
                 sh 'export DOCKER_HOST=tcp://3.234.205.203:2375'
-                sh 'docker -H tcp://3.234.205.203:2375 ps'
-                sh 'docker build -t maven:$BUILD_ID'
+                sh '''
+                    docker -H tcp://3.234.205.203:2375 ps
+                    docker -H tcp://3.234.205.203:2375 build -t maven:$BUILD_ID -f Dockerfile .
+                    docker -H tcp://3.234.205.203:2375 tag maven:$BUILD_ID sbhalsing0/maven:$BUILD_ID 
+                    docker -H tcp://3.234.205.203:2375 push sbhalsing0/maven:$BUILD_ID
+                    kubectl version --client
+                    sed -i "s/tag/${BUILD_ID}/g" k8s-deployment.yaml
+                    cat  k8s-deployment.yaml
+                    kubectl apply -f k8s-deployment.yaml
+                    kubectl get pods
+                '''
             }
         }
     }
