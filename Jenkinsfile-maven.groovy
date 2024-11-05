64bd1e7511f74648b215b3fe5b87da3db3b3d709
Sanket Bhalsing <42617962+Sbhalsing0@users.noreply.github.com>
2024-11-05 22:01:50 +0530

    Update Jenkinsfile-maven.groovy

--- Jenkinsfile-maven.groovy
+++ Jenkinsfile-maven.groovy
@@ -26,12 +26,28 @@ pipeline {
         """
       }
   }
+  environment {
+        DOCKER_USERNAME = credentials('docker-id') // Replace with your Docker Hub username credential ID
+        DOCKER_PASSWORD = credentials('docker-id') // Replace with your Docker Hub password credential ID
+  }
   options {
+    ansiColor('xterm')
     timestamps()
     buildDiscarder(logRotator(numToKeepStr: '40', daysToKeepStr: '14', artifactDaysToKeepStr: '7', artifactNumToKeepStr: '10'))
     timeout(time: 3, unit:'HOURS')
   }
     stages {
+        stage('Login to Docker Hub') {
+            steps {
+                withCredentials([string(credentialsId: 'docker-creds', variable: 'DOCKER_PASSWORD')]) {
+                    script {
+                        // Login to Docker Hub using PAT
+                        sh 'export DOCKER_HOST=tcp://44.200.159.148:2375'
+                        sh 'docker -H tcp://44.200.159.148:2375 login -u sbhalsing0 -p $DOCKER_PASSWORD'
+                    }
+                }
+            }
+        }
         stage('Build') {
             steps {
                 script {
@@ -41,23 +57,32 @@ pipeline {
                 sh '''
                 echo $BUILD_ID
                 mvn clean install -DskipTests=false
+                mvn package fabric8:build
                 '''
             }
         }
         stage('Release') {
             steps {
-                sh 'export DOCKER_HOST=tcp://3.234.205.203:2375'
-                sh '''
-                    docker -H tcp://3.234.205.203:2375 ps
-                    docker -H tcp://3.234.205.203:2375 build -t maven:$BUILD_ID -f Dockerfile .
-                    docker -H tcp://3.234.205.203:2375 tag maven:$BUILD_ID sbhalsing0/maven:$BUILD_ID 
-                    docker -H tcp://3.234.205.203:2375 push sbhalsing0/maven:$BUILD_ID
-                    kubectl version --client
-                    sed -i "s/tag/${BUILD_ID}/g" k8s-deployment.yaml
-                    cat  k8s-deployment.yaml
-                    kubectl apply -f k8s-deployment.yaml
-                    kubectl get pods
-                '''
+                script {
+                    withCredentials([usernamePassword(credentialsId: 'docker-id', 
+                            usernameVariable: 'DOCKER_USERNAME', 
+                            passwordVariable: 'DOCKER_PASSWORD')]) {
+                        sh 'echo $DOCKER_USERNAME'
+                        sh 'echo $DOCKER_PASSWORD'                            
+                    }
+                     sh 'export DOCKER_HOST=tcp://44.200.159.148:2375'
+                     sh '''
+                        docker -H tcp://44.200.159.148:2375 ps
+                        docker -H tcp://44.200.159.148:2375 build -t maven:$BUILD_ID -f Dockerfile .
+                        docker -H tcp://44.200.159.148:2375 tag maven:$BUILD_ID sbhalsing0/maven:$BUILD_ID 
+                        docker -H tcp://44.200.159.148:2375 push sbhalsing0/maven:$BUILD_ID
+                        kubectl version --client
+                        sed -i "s/tag/${BUILD_ID}/g" k8s-deployment.yaml
+                        cat  k8s-deployment.yaml
+                        kubectl apply -f k8s-deployment.yaml
+                        kubectl get pods
+                    '''
+                }
             }
         }
     }
